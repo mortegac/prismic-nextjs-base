5b27eee89ef01f8d3ae56f66d7f6b1dbaf783d43
Manuel Ortega <mortega@apgca.cl>
2022-07-08 15:01:55 -0400

    chore: add @netlify/plugin-nextjs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["images.prismic.io", "images.unsplash.com"],
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 module.exports = nextConfig
